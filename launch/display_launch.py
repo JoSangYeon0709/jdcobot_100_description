34f76abdf30e6f439efb92d72d5d23e0b210b0bb
jsy <whtkddus159@naver.com>
2024-05-18 20:32:43 +0900

    jdcobot100 v1

---
+++ launch/display_launch.py
@@ -0,0 +1,65 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf = os.path.join(get_package_share_directory('jdcobot_100_description'), 'urdf', 'jdcobot_100_description.urdf')
+    rviz_config = os.path.join(get_package_share_directory('jdcobot_100_description'), 'rviz', 'rviz_set.rviz')
+
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+            parameters=[{
+		'use_sim_time': use_sim_time,
+		'robot_description': robot_desc,
+		'source_list': ['joint_1', 'joint_2', 'joint_3', 'joint_4'],
+		'zeros': {
+		    'joint_1': 1.5708,
+		    'joint_2': 1.5708,
+		    'joint_3': 1.5708,
+		    'joint_4': 1.5708
+		}
+	    }]
+        ),
+	Node(
+	    package='robot_state_publisher',
+	    executable='robot_state_publisher',
+	    name='robot_state_publisher',
+	    output='screen',
+	    parameters=[{
+		'use_sim_time': use_sim_time,
+		'robot_description': robot_desc,
+		'source_list': ['joint_1', 'joint_2', 'joint_3', 'joint_4'],
+		'zeros': {
+		    'joint_1': 1.5708,
+		    'joint_2': 1.5708,
+		    'joint_3': 1.5708,
+		    'joint_4': 1.5708
+		}
+	    }]
+	),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('jdcobot_100_description'),'rviz','rviz_set.rviz')]),
+    ])
+
+
+if __name__ == '__main__':
+    generate_launch_description()
